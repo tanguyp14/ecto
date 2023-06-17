aa4ae07e97cd68408e488476c6e47d5a15945c6f
Tanguy Pinel <tanguypinel@gmail.com>
2023-06-17 11:22:28 +0200

    Update

--- header.php
+++ header.php
@@ -18,11 +18,11 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/site.webmanifest">
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff316f">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri() ?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri() ?>/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo get_stylesheet_directory_uri() ?>/site.webmanifest">
+	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri() ?>/safari-pinned-tab.svg" color="#ff316f">
 	<meta name="msapplication-TileColor" content="#ffeec1">
 	<meta name="theme-color" content="#ffeec1">
 
