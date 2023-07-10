d6977b64ae6324b03cc69f650e15acca401822ce
Tanguy Pinel <tanguypinel@gmail.com>
2023-07-10 12:14:15 +0200

    Send

---
+++ acf-blocks/arranges/view.php
@@ -0,0 +1,59 @@
+<section class="arranges">
+    <?php
+    $fields = get_fields();
+    extract($fields);
+    ?>
+    <h2><?php echo $titre; ?></h2>
+    <div class="rhum">
+        <?php extract($rhum); ?>
+        <div class="prix">
+            <div class=" prix_left">
+                <p>3cl</p>
+                <p class="p"><?php echo $prix_3cl; ?>€</p>
+            </div>
+            <div class="prix_right">
+                <p>5cl</p>
+                <p class="p"><?php echo $prix_5cl; ?>€</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <ul>
+                <?php foreach ($liste as $r) : extract($r) ?>
+                    <li><?php echo $recette; ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <article class="ti-punch">
+                <div class="attributes">
+                    <h3>Ti Punch</h3>
+                    <span class="type">5cl</span>
+                    <span><?php echo $ti_punch; ?>€</span>
+                </div>
+            </article>
+        </div>
+    </div>
+    <div class="calva">
+        <?php extract($calva); ?>
+        <?php extract($rhum); //Clone 
+        ?>
+        <div class="prix">
+            <div class=" prix_left">
+                <p>3cl</p>
+                <p class="p"><?php echo $prix_3cl; ?>€</p>
+            </div>
+            <div class="prix_right">
+                <p>5cl</p>
+                <p class="p"><?php echo $prix_5cl; ?>€</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <ul>
+
+                <?php foreach ($liste as $c) : extract($c) ?>
+                    <li><?php echo $recette; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+</section>
