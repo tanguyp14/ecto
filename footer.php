0732cc0a9f11a8efb5c3811cfeb61e609cf7623d
Tanguy Pinel <tanguypinel@gmail.com>
2023-07-14 15:32:41 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,20 +1,30 @@
 <a href="#masthead" class="back-top hide" alt="Back to top" title="Retour en haut de page"></a>
-		</main><!-- /#main-content -->
+</main><!-- /#main-content -->
 
-		<footer role="contentinfo">
-			<div class="bottom-bar">
-				<p>	<?php $struct = get_field('structure', 'option');
+<footer role="contentinfo">
+	<div class="pre-footer"><span></span></div>
+	<div class="bottom-bar">
+		<span> <?php $struct = get_field('structure', 'option');
 				extract($struct); ?>
-					&copy; <?php echo $denomination ?> - <?php echo date("Y"); ?> -
-					<?php $pixelea = get_field('creation_developpement_site', 'option');
-					extract($pixelea); ?>
-					<?php $field = get_field_object('creation_developpement_site', 'option'); ?>
-					<?php echo $field['label']; ?> :
-					<a href="<?= $site_web ?>"> <?= $denomination ?></a>
-				</p>
-			</div>
-		</footer>
-		<?php wp_footer(); ?>
-		</body>
+			&nbsp;- &copy; <?php echo $denomination ?> - <?php echo date("Y"); ?> -
+			<?php $hatsu = get_field('creation_developpement_site', 'option');
+			extract($hatsu); ?>
+			<?php $field = get_field_object('creation_developpement_site', 'option'); ?>
+			<?php echo $field['label']; ?> :
+			<a href="<?= $site_web ?>"> <?= $denomination ?></a>
+		</span>
+		<span> <?php $struct = get_field('structure', 'option');
+				extract($struct); ?>
+			&nbsp;- &copy; <?php echo $denomination ?> - <?php echo date("Y"); ?> -
+			<?php $hatsu = get_field('creation_developpement_site', 'option');
+			extract($hatsu); ?>
+			<?php $field = get_field_object('creation_developpement_site', 'option'); ?>
+			<?php echo $field['label']; ?> :
+			<a href="<?= $site_web ?>"> <?= $denomination ?></a>
+		</span>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
 
-		</html>
+</html>
