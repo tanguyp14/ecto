9f717f36ec551ac59a896904e472d147bec09778
Tanguy Pinel <tanguypinel@gmail.com>
2023-07-15 13:45:44 +0200

    ajustement CSS

---
+++ acf-blocks/contact/view.php
@@ -0,0 +1,10 @@
+<section class="contact">
+    <?php $struct = get_field('structure', 'option');
+    extract($struct); ?>
+    <h4><?php echo $denomination; ?></h4>
+    <span>Apéritif de qualité depuis 2014</span>
+    <span class="blue"><?php echo $adresse; ?></span>
+    <span><?php echo $code_postal; ?> - <?php echo $commune; ?></span>
+    <span class="blue">14h - 01h</span>
+    <span>Fin du service à 00h30</span>
+</section>
