83ca77d4c9189c88b442527fdc3aeda5c64a9810
Tanguy Pinel <tanguypinel@gmail.com>
2025-01-08 12:40:21 +0100

    Ajout de nouveaux fichiers CSS et JS, réécriture de app.js et mise à jour des styles SCSS pour les blocs de bière

---
+++ dist/js/app.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+	$('document').ready(function () {
+		//		Burger nav
+		$('.menu-toggle').on('click', function () {
+			$('header').toggleClass('header--menu-is-open');
+		});
+
+		$('nav li').on('click', function () {
+			$('header').toggleClass('header--menu-is-open');
+		});
+	});
+
+	$(window).scroll(function () {
+		if ($(document).scrollTop() < 700) {
+			$('.back-top').addClass('hide');
+		}
+		else {
+			$('.back-top').removeClass('hide');
+		}
+	});
+
+}(jQuery));
+//# sourceMappingURL=app.js.map
